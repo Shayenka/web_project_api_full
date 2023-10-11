08b83b542ba2c5d97ed5ac5d29a018f172b89a0d
Shayenka Alvarado <shayenkalvarado@gmail.com>
2023-10-10 20:52:06 -0600

    Proyect 16

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,4 +1,4 @@
-const jwt = require('sonwebtokenj');
+const jwt = require('jsonwebtoken');
 
 const generateAuthToken = (data) => {
   const token = jwt.sign({ _id: data._id }, process.env.SECRET_KEY, { expiresIn: '7d' });
